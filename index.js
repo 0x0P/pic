f4c4943424a0925ef6bf1e8293314488e7745bb1
_0x0_P <69731703+0x0P@users.noreply.github.com>
2023-03-05 11:59:58 +0900

    클래스 변경

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 document.querySelector("#bo_v_atc").style.maxHeight = "100%";
-document.querySelector(".continue_reading").remove();
+document.querySelector(".continue_coupang") ? document.querySelector(".continue_coupang").remove() : console.log(null)
